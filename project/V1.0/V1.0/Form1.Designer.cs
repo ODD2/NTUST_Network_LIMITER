955010b84bc91116ec5d4127184dce2729d3d706
OD_ISME <vhanxtreme@gmail.com>
2018-02-26 23:09:23 +0800

    Merge to SourceTree

---
+++ project/V1.0/V1.0/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace V1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(434, 227);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(79, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "無限制";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox1_PreviewKeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(27, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2, 14);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Harlow Solid Italic", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Location = new System.Drawing.Point(12, 235);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.label3.Size = new System.Drawing.Size(217, 115);
+            this.label3.TabIndex = 3;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Harlow Solid Italic", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(224, 235);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.label4.Size = new System.Drawing.Size(222, 115);
+            this.label4.TabIndex = 4;
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(16, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 21);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "流量(kb/s):";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(198, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 21);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "總流量(GB):";
+            this.label6.Visible = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 357);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "台科網路限制器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ project/V1.0/V1.0/Form1.cs
@@ -0,0 +1,517 @@
+﻿#define ODDDEBUG_Lv1
+//#define ODDDEBUG_LV2
+using System;
+using System.Windows.Forms;
+using System;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.Sockets;
+using System.Timers;
+using System.IO;
+
+
+namespace V1._0
+{
+    public partial class Form1 : Form
+    {
+
+        IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+        NetworkInterface adapter;
+        string NoNICmsg = "沒有找到連接台科網路的網卡。\n請檢查網路裝置後再使用本軟體\n";
+
+        string folder_location = Environment.GetEnvironmentVariable("userprofile") + "\\Documents\\" + "\\ntustnet\\";
+
+        string customTimefmt = "yyyy/MM/dd-HH:mm:ss.";
+        string debugTimefmt = "MM/dd-HH:mm:ss.";
+
+        DateTime NetCurrentTime;
+
+        double Restrict = 0.0;
+        double Runner = 0.0;
+        double DeltaRunner = 0.0;
+        double Start = 0.0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            notifyIcon1.Icon = this.Icon;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            //初始化
+            //Textbox1的外觀
+            textBox1.BorderStyle = BorderStyle.Fixed3D;
+
+            label1.Text += computerProperties.HostName + "." + computerProperties.DomainName;
+            //顯示沒有網卡
+            if (nics == null || nics.Length < 1)
+            {
+                label1.Text += "沒有找到網卡。\n";
+                label1.Text += NoNICmsg;
+                return;
+            }
+
+
+            label1.Text += "的網卡資訊:(找到" + nics.Length + "張網卡)\n";
+            int adap_index = 1;
+
+            //列出所有網卡
+            label1.Text += "==========================================\n";
+            foreach (NetworkInterface i in nics)
+            {
+                label1.Text += "  (" + adap_index + ")->" + i.Name + "\n";
+                adap_index += 1;
+            }
+            label1.Text += "==========================================\n";
+
+            //偵測台科網路用的網卡
+            adap_index = 0;
+            foreach (NetworkInterface i in nics)
+            {
+
+                if (i.OperationalStatus.ToString() == "Up")
+                {
+                    if (String.Compare(i.GetIPProperties().UnicastAddresses[1].Address.ToString().Substring(0, 7), "140.118") == 0)
+                    {
+                        label1.Text += "找到台科網卡:\n";
+                        label1.Text += "    [" + i.Name + "]----[台科網路]\n";
+                        adapter = i;
+                        break;
+                    }
+                }
+                adap_index += 1;
+            }
+            label1.Text += "==========================================\n";
+
+
+            //沒有找到台科網卡的狀況
+            if (adap_index == nics.Length)
+            {
+                label1.Text += NoNICmsg;
+                return;
+            }
+
+
+            //輸入流量
+            label1.Text += "流量限制:                     (輸入完請按enter)\n";
+            label1.Text += "==========================================\n";
+            textBox1.Visible = true;
+
+
+            //確保需要的檔案存在
+            CheckFileFolder();
+
+
+            //到台科流量網站以及本地檔案更新目前總流量
+            RunnerInitializer();
+#if (ODDDEBUG_Lv1)
+            WriteDebugToFileLog(GetNetworkTime().ToString(debugTimefmt), "Initialized Runner:" + Runner.ToString() + "\n");
+#endif
+
+            //更新本機網卡的流量基礎
+            InitInterfaceStatisticBase();
+
+            //啟用限流核心
+            timer1.Enabled = true;
+
+            
+            //啟動下面板的顯示
+            label5.Visible = true;
+            label6.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+
+            return;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BackupStatisticToFile();
+            WriteDebugToFileLog(GetNetworkTime().ToString(debugTimefmt), "Application Closed!");
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+            else if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.notifyIcon1.Visible = false;
+        }
+
+
+        /// <summary>
+        /// TEXTBOX
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.BackColor = System.Drawing.Color.Red;
+            textBox1.BorderStyle = BorderStyle.None;
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == '\r')
+            {
+                //textBox1.BorderStyle = BorderStyle.Fixed3D;
+                try
+                {
+                    Restrict = Convert.ToDouble(textBox1.Text);
+
+                }
+                catch (Exception e_2)
+                {
+                    WriteDebugToFileLog("Error,Wrong Float Number Format!!\n");
+                    Restrict = 0.0;
+                }
+
+                if (Restrict == 0.0)
+                {
+                    textBox1.Text = "無限制";
+                }
+                //每次使用者按下enter就更新流量上限
+                textBox1.BackColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void textBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar < '0' || e.KeyChar > '9')
+            {
+                if (e.KeyChar == '\r' || e.KeyChar == '.' || e.KeyChar == '\b') ;
+                else e.Handled = true;
+            }
+        }
+
+
+        /// <summary>
+        /// TIMER(CORE)
+        /// </summary>
+        int tick = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Initialize Runner Every Time A Day Passed.
+            if (NetCurrentTime.Hour == 23 && (NetCurrentTime.Minute + (NetCurrentTime.Second + tick) / 60) == 59 && (NetCurrentTime.Second + tick) % 60 == 59)
+            {
+                Runner = 0.0;
+                WriteDebugToFileLog(GetNetworkTime().ToString(debugTimefmt), "Day Changed!");
+            }
+
+            //Update Runner And DeltaRunner
+            UpdateInterfaceStatistics();
+
+            //Restrict the Network Flow only When There's a Restrict
+            if (Restrict != 0)
+            {
+                if (Runner >= Restrict - 0.1)
+                {
+#if (ODDDEBUG_Lv1)
+                    WriteDebugToFileLog(GetNetworkTime().ToString(debugTimefmt), "MEET RESTRICT LIMIT:" + Runner.ToString() + "\n");
+#endif
+                    DisableInterface(adapter.Name);
+
+                    timer1.Enabled = false;
+                    BackupStatisticToFile();
+                    return;
+                }
+            }
+
+
+
+            //tick 以秒計算，每30秒備份到檔案裡.
+            tick += 1;
+            if (tick % 1800 == 0 || DeltaRunner >= 10000.0)
+            {
+                BackupStatisticToFile();
+                tick = 0;
+            }
+        }
+
+
+
+        /// <summary>
+        /// STATISTIC 
+        /// </summary>
+        private void UpdateInterfaceStatistics()
+        {
+#if (ODDDEBUG_Lv2)
+            WriteDebugToFileLog("RUNNER:" + Runner.ToString() + "\n");
+#endif
+
+            DeltaRunner = GetInterfaceStatistics() - Start;
+            Start += DeltaRunner;
+            Runner += DeltaRunner;
+            RunnersDisplay();
+        }
+
+        private double GetInterfaceStatistics()
+        {
+            double RecGB = (double)adapter.GetIPv4Statistics().BytesReceived / (1024 * 1024 * 1024);
+            double SendGB = (double)adapter.GetIPv4Statistics().BytesSent / (1024 * 1024 * 1024);
+            return RecGB + SendGB;
+        }
+
+        private void InitInterfaceStatisticBase()
+        {
+            Start = GetInterfaceStatistics();
+#if (ODDDEBUG_Lv1)
+            WriteDebugToFileLog(GetNetworkTime().ToString(debugTimefmt), "Initialized Start:" + Start.ToString() + "\n");
+#endif
+        }
+
+
+        /// <summary>
+        /// Runner Initialization
+        /// </summary>
+        /// 
+        private void RunnerInitializer()
+        {
+            double http = UpdateRealTotalFromhttp();
+            double file = UpdateRealTotalFromFile();
+            Runner = file > http ? file : http;
+        }
+
+        private double UpdateRealTotalFromhttp()
+        {
+            WebClient client = new WebClient();
+
+            Stream data = client.OpenRead("https://network.ntust.edu.tw/");
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            int index = s.IndexOf("總計");
+            for (int i = 0; i < 4; i++)
+            {
+                index = s.IndexOf("<td>", index);
+                index += 4;
+            }
+
+            while (!(s[index] >= 48 && s[index] <= 57)) index += 1;
+            string total_KB_string = "";
+            while (s[index] != ' ')
+            {
+                if (s[index] >= '0' && s[index] <= '9')
+                {
+                    total_KB_string += s[index];
+                }
+                index += 1;
+            }
+            return Convert.ToDouble(total_KB_string) / (1024 * 1024);
+        }
+
+        private double UpdateRealTotalFromFile()
+        {
+            string[] STAT_TXT = File.ReadAllLines(folder_location + "STAT.txt");
+            //如果是新建立的檔案
+            if (STAT_TXT.Length == 0) { return 0.0; }
+
+
+
+            //如果不是新建立的檔案
+
+            NetCurrentTime = GetNetworkTime();
+            if (String.Compare(NetCurrentTime.ToString(customTimefmt), STAT_TXT[0]) < 0)
+            {
+#if (ODDDEBUG_Lv1)
+                WriteDebugToFileLog(GetNetworkTime().ToString(debugTimefmt), "File Date is Faster than Current Date!!" + NetCurrentTime.ToString(customTimefmt) + "<" + STAT_TXT[0] + "\n");
+#endif
+                return 0.0;
+            }
+            else if (String.Compare(NetCurrentTime.ToString(customTimefmt).Substring(0, 10), STAT_TXT[0].Substring(0, 10)) > 0)
+            {
+                //如果檔案的日期比較舊，就回傳0
+                return 0.0;
+            }
+            else
+            {
+                return Convert.ToDouble(STAT_TXT[1]);
+            }
+        }
+
+        /// <summary>
+        /// FILEIO
+        /// </summary>
+        private void CheckFileFolder()
+        {
+            if (!Directory.Exists(folder_location))
+                Directory.CreateDirectory(folder_location);
+            if (!File.Exists(folder_location + "LOG.txt"))
+                using (File.CreateText(folder_location + "LOG.txt")) { }
+            if (!File.Exists(folder_location + "STAT.txt"))
+                using (File.CreateText(folder_location + "STAT.txt")) { }
+        }
+
+        private void BackupStatisticToFile()
+        {
+            NetCurrentTime = GetNetworkTime();
+            string Content = NetCurrentTime.ToString(customTimefmt) + "\n" + Runner.ToString();
+            File.WriteAllText(folder_location + "STAT.txt", Content);
+        }
+
+        private void WriteDebugToFileLog(string msg)
+        {
+            using (StreamWriter sw = File.AppendText(folder_location + "LOG.txt"))
+            {
+                sw.Write(msg);
+            }
+        }
+
+        private void WriteDebugToFileLog(string time, string msg)
+        {
+            using (StreamWriter sw = File.AppendText(folder_location + "LOG.txt"))
+            {
+                sw.Write(time + "\n" + "     " + msg);
+            }
+        }
+
+        /// <summary>
+        /// Interface Control
+        /// </summary>
+        /// <param name="name"></param>
+        private void DisableInterface(string name)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C netsh interface set interface name=\"" + name + "\" admin=DISABLE";
+            process.StartInfo = startInfo;
+            process.Start();
+
+        }
+
+        private void EnableInterface(string name)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C netsh interface set interface name=\"" + name + "\" admin=ENABLE";
+            process.StartInfo = startInfo;
+            process.Start();
+
+        }
+
+
+        /// <summary>
+        /// Display
+        /// </summary>
+        private void RunnersDisplay()
+        {
+            double DeltaInKb = DeltaRunner * 1000 * 1000;
+            if (this.Visible == true)
+            {
+                label3.Text = Math.Round(DeltaInKb, 2).ToString();
+                label4.Text = Math.Round(Runner, 2).ToString();
+            }
+            else if (notifyIcon1.Visible == true)
+            {
+                notifyIcon1.Text = "限制: " + textBox1.Text+"(GB)\n"+
+                                   "目前: " + Math.Round(Runner, 2).ToString()+"(GB)";
+            }
+        }
+
+
+
+        /// <summary>
+        /// Time Server Get Time;
+        /// </summary>
+        /// <returns></returns>
+        private DateTime GetNetworkTime()
+        {
+            //default Windows time server
+            const string ntpServer = "time.windows.com";
+
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            var ntpData = new byte[48];
+
+            //Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+            //The UDP port number assigned to NTP is 123
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            //NTP uses UDP
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                try
+                {
+                    socket.Connect(ipEndPoint);
+
+                    //Stops code hang if NTP is blocked
+                    socket.ReceiveTimeout = 3000;
+
+                    socket.Send(ntpData);
+                    socket.Receive(ntpData);
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    WriteDebugToFileLog("Error to Catch Network Time!!" + "\n");
+                    return NetCurrentTime;
+                }
+            }
+
+            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            //departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
+
+            //Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+            //Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+            //Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            //**UTC** time
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime.ToLocalTime();
+        }
+
+        private uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
+        }
+
+        
+    }
+}
